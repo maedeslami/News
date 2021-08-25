2efff55b11c88d301f6c52f234221344cc3bd81b
maedeslami <maedeslami@yahoo.com>
2021-08-25 15:34:02 -0700

    rest template to listener

--- src/main/java/news/service/NewsServiceImpl.java
+++ src/main/java/news/service/NewsServiceImpl.java
@@ -3,9 +3,12 @@ package news.service;
 import news.model.News;
 import news.repository.JiringDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -25,8 +28,12 @@ public class NewsServiceImpl implements NewsService {
             news.setHeadline("upGood");
             Thread thread = new Thread(news);
             thread.start();
+            //ToDo rest template to analyser
+            String url = "http://localhost:8085";
+            RestTemplate restTemplate = new RestTemplate();
+            News object = restTemplate.postForObject(url, news, News.class);
             System.out.println(news.getHeadline() +  "       " + "this is a headline");
-            return jiringDao.save(news);
+            return jiringDao.save(object);
         }
 
     }
